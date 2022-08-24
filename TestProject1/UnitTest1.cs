6918dd7fac691d0e14431145de0919840452618f
ETG1 <ETG1@6BBYQG2-SHEL>
2022-08-24 17:21:33 +0530

    Add project files.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jenkins;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class TestCalculator
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            Calculator c = new Calculator();
+            int result = c.Add(10, 20);
+            Assert.AreEqual(30, result);
+        }
+    }
+}
